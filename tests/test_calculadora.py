191e98632baf3732b2ed7a52abc7ee7b3b50e5cb
TiagoBertoline <tiago.bertoline@hotmail.com>
2026-05-25 13:14:14 -0300

    Fix: corrigindo teste de soma.

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -2,7 +2,7 @@ import pytest
 from src.calculadora import somar, dividir
 
 def test_somar():
-    assert somar(2, 3) == 99
+    assert somar(2, 3) == 5
     assert somar(-1, 1) == 0
 
 def test_dividir():
